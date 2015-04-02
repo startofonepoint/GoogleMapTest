0179fa42076a0151558fcd6a422f092cfe9a392b
startofonepoint <startofonpoint@gmail.com>
2015-04-02 15:37:37 +0900

    FirstCommit

--- GoogleMapTutorial/AppDelegate.swift
+++ GoogleMapTutorial/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyDoGG9LDTBl0LnOLMoBznvhyB_mJwzpvu4")
         return true
     }
 
--- GoogleMapTutorial/ViewController.swift
+++ GoogleMapTutorial/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var camera = GMSCameraPosition.cameraWithLatitude(-33.86, longitude: 151.2, zoom: 6.0)
+        var mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        self.view = mapView
+        
+        var marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.2)
+        marker.title = "Sydney"
+        marker.snippet = "Austrailia"
+        marker.map = mapView
     }
 
     override func didReceiveMemoryWarning() {
